7ab938ed5b0a3b2ac17000ff770b3b31c108fe87
Kinson261 <80491985+Kinson261@users.noreply.github.com>
2021-08-06 11:32:42 +0300

    adding selecting character features

---
+++ Drone_VIS/Assets/characterSelect.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class characterSelect : MonoBehaviour
+{
+    public GameObject[] Agents;
+    public GameObject currentAgent;
+
+    private int i;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activeAgent();
+    }
+
+    public void activeAgent()
+    {
+        currentAgent = (GameObject)Selection.activeObject;
+
+        for (i=0;  i < Agents.Length; i++)
+        {
+            if (currentAgent != Agents[i])
+            {
+                Agents[i].SetActive(false);
+            }
+            else
+            {
+                Agents[i].SetActive(true);
+            }
+        }
+    }
+}
