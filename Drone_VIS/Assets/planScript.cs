c22b59ccde5395a50719eb6310fe88cc8f8ed74d
Kinson261 <80491985+Kinson261@users.noreply.github.com>
2021-08-15 06:06:30 +0300

    Update planScript.cs

--- Drone_VIS/Assets/planScript.cs
+++ Drone_VIS/Assets/planScript.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public class planScript : MonoBehaviour
 {
-    private GameObject plan;
+    public GameObject plan;
     public int sizeX;
     public int sizeY;
     public int sizeZ;
 
+    public float boundX;
+    public float boundY;
+    public float boundZ;
+
+
+    public float surfacePlane;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        plan = GameObject.Find("Plan");
+        //plan = GameObject.Find("Plane");
         plan.transform.position = new Vector3(0, 0, 0);
-        plan.transform.localScale = new Vector3(30, 30, 30);
+        plan.transform.localScale = new Vector3(10, 10, 10);
+
+        calculateSurfacePlane();
+        calculateBoundaries();
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         plan.transform.localScale = new Vector3(sizeX, sizeY, sizeZ);
+        calculateSurfacePlane();
+        calculateBoundaries();
     }
 
 
+    public void calculateSurfacePlane()
+    {
+        surfacePlane = (plan.transform.localScale.x * 10f) * (plan.transform.localScale.z * 10f);
+    }
+
+
+    public void calculateBoundaries()
+    {
+        boundX = (plan.transform.localScale.x * 10f) / 2f;
+        boundY = (plan.transform.localScale.y * 10f) / 2f;
+        boundZ = (plan.transform.localScale.z * 10f) / 2f;
+    }
 }
