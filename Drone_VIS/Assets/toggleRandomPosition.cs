46f2694a521d958d821aba158c5fcc0f2e540d6b
Kinson261 <80491985+Kinson261@users.noreply.github.com>
2021-08-15 22:31:26 +0300

    added script for toggle button

---
+++ Drone_VIS/Assets/toggleRandomPosition.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class toggleRandomPosition : MonoBehaviour
+{
+    public Toggle m_Toggle;
+    public bool random;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Toggle = GetComponent<Toggle>();
+        random = m_Toggle.isOn;
+        //Add listener for when the state of the Toggle changes, and output the state
+        m_Toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(m_Toggle);
+        });
+
+    }
+
+    public void ToggleValueChanged(Toggle m_Toggle)
+    {
+        random =  m_Toggle.isOn;
+    }
+}
