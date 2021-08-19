376b2b4039f23e70bcd08a9d41ee842e04d2994f
Kinson261 <80491985+Kinson261@users.noreply.github.com>
2021-08-19 22:25:19 +0300

    Updated "onValueChanged" to "onEndEdit" and added comments

--- Drone_VIS/Assets/dropdownValueDrones.cs
+++ Drone_VIS/Assets/dropdownValueDrones.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class dropdownValueDrones : MonoBehaviour
@@ -8,16 +9,14 @@ public class dropdownValueDrones : MonoBehaviour
     [Space]
     [Space]
     public planScript PlanScript;
-    public dropdownHandler dropdownScript;
     public List<GameObject> allAgents;
 
     [Space]
     [Space]
-    private GameObject[] drones = new GameObject[20];
+    private GameObject[] drones = new GameObject[20];           //limit drone number to 20
     private GameObject objectToCopy;
     private GameObject objectToDestroy;
 
-    //public Dropdown dropdown;
     public Dropdown m_Dropdown;
     public int m_DropdownValue;
     private int i;
@@ -27,16 +26,18 @@ public class dropdownValueDrones : MonoBehaviour
     [Space]
     [Space]
     public Toggle m_Toggle;
-    public bool custom;
+    public bool custom;                             //custom spawn point
 
     [Space]
     [Space]
+    //create InputField objects
     public InputField inputFieldX;
     public InputField inputFieldY;
     public InputField inputFieldZ;
 
     [Space]
     [Space]
+    //Vector for given position
     public Vector3 pos;
     private float initPosX;
     private float initPosY;
@@ -44,6 +45,7 @@ public class dropdownValueDrones : MonoBehaviour
 
     [Space]
     [Space]
+    //var with random values
     private float randPosX;
     private float randPosY;
     private float randPosZ;
@@ -51,46 +53,43 @@ public class dropdownValueDrones : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        //dropdownScript = GameObject.Find("DropdownID").GetComponent<dropdownHandler>();
-        //allAgents = dropdownScript.allAgents;
-
+        //init
         inputFieldX.text = "X";
         inputFieldY.text = "Y";
         inputFieldZ.text = "Z";
+        inputFieldX.interactable = false;
+        inputFieldY.interactable = false;
+        inputFieldZ.interactable = false;
 
         //Adds a listener to the main input field and invokes a method when the value changes.
-        inputFieldX.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
-        inputFieldY.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
-        inputFieldZ.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        inputFieldX.onEndEdit.AddListener(delegate { ValueChangeCheck(); });
+        inputFieldY.onEndEdit.AddListener(delegate { ValueChangeCheck(); });
+        inputFieldZ.onEndEdit.AddListener(delegate { ValueChangeCheck(); });
 
 
-        pos = new Vector3(initPosX, initPosY, initPosZ);
-
-
-        objectToCopy = GameObject.Find("MAV0");
-
-
-        determineI();
-
+        //Adds a listener to the dropdown and invokes a method when the value changes.
         m_Dropdown.onValueChanged.AddListener(delegate
         {
             DropdownValueChanged(m_Dropdown);
         });
 
 
-
         m_Toggle = GameObject.Find("ToggleDrone").GetComponent<Toggle>();
         custom = m_Toggle.isOn;
-
         //Add listener for when the state of the Toggle changes, and output the state
         m_Toggle.onValueChanged.AddListener(delegate {
             ToggleValueChanged(m_Toggle);
         });
 
+
+        pos = new Vector3(initPosX, initPosY, initPosZ);    //given coordinates
+        objectToCopy = GameObject.Find("MAV0");             //sample
+        determineI();                                       
+
     }
 
 
+    //function for converting string to float
     public void ValueChangeCheck()
     {
         float.TryParse(inputFieldX.text, out initPosX);
@@ -99,7 +98,7 @@ public class dropdownValueDrones : MonoBehaviour
 
     }
 
-
+    //function to evaluate the state of toggle button
     public void ToggleValueChanged(Toggle m_Toggle)
     {
         custom = m_Toggle.isOn;
@@ -110,23 +109,27 @@ public class dropdownValueDrones : MonoBehaviour
     {
 
         determineI();
-        iMax = (int)m_Dropdown.value;
+        iMax = (int)m_Dropdown.value;           //new value
         ToggleValueChanged(m_Toggle);
         Duplicate();
         Delete();
     }
 
 
+
+    //function to generate drones
     public void Duplicate()
     {
         for (i = NB.Length; i <= iMax; i++)
         {
+            //generate random numbers within the limits of the plan
             randPosX = Random.Range(-PlanScript.boundX, PlanScript.boundX);
             randPosY = Random.Range(-PlanScript.boundY, PlanScript.boundY);
             randPosZ = Random.Range(-PlanScript.boundZ, PlanScript.boundZ);
 
+            
+            drones[i] = Instantiate(objectToCopy);          //generate drones
             custom = m_Toggle.isOn;
-            drones[i] = GameObject.Instantiate(objectToCopy);
 
             if (custom == false)
             {
@@ -135,31 +138,29 @@ public class dropdownValueDrones : MonoBehaviour
             else
             {
                 drones[i].transform.position = new Vector3(initPosX, initPosY, initPosZ);
-                initPosX = initPosX + 3f;
+                initPosX = initPosX + 3f;                   //shifting to give space to the next drone
             }
            
             drones[i].transform.rotation = Quaternion.identity;
             drones[i].name = "MAV" + i;
 
-
-            /////////////////////////
-            //allAgents.Add(drones[i]);
-            /////////////////////////
         }
 
     }
 
+
+    //function to delete drones
     public void Delete()
     {
         for (iMax = m_Dropdown.value; iMax <= i; iMax++)
         {
             objectToDestroy = GameObject.Find("MAV" + iMax.ToString());
             Destroy(objectToDestroy);
-            //allAgents.Remove(drones[iMax]);
         }
     }
 
 
+    //function to evaluate how many drones are on the scene
     public void determineI()
     {
         NB = GameObject.FindGameObjectsWithTag("Drone");
--- Drone_VIS/Assets/playerController.cs
+++ Drone_VIS/Assets/playerController.cs
@@ -4,76 +4,122 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
-using UnityEditor;
 
 public class playerController : MonoBehaviour
 {
-    public NavMeshAgent agent;
-    public GameObject currentSelection;
-    public NavMeshAgent agentCurrentSelection;
-    public dropdownHandler dropdownHandler;
+    private GameObject currentSelection;
+    public selectionHandler selectionHandler;
 
     [Space]
     [Space]
+    //Create InputField objects
     public InputField inputFieldTargetX;
     public InputField inputFieldTargetY;
     public InputField inputFieldTargetZ;
 
     [Space]
     [Space]
+    //Associate a float to each inputfield
     private float SetPosX;
     private float SetPosY;
     private float SetPosZ;
-
     public Vector3 SetPos;
 
     [Space]
     [Space]
+    //Create a toggle object
     public Toggle m_Toggle;
-    public bool target;
+    public bool target;                     //target is on
+
+    [Space]
+    [Space]
+    public float maxSpeedMobileRobot;   
+    //public float maxSpeedDrone;
+
+    /*
+     maxSpeedDrone IS NOT NEEDED SINCE WE DON'T USE NAV MESH TO CONTROL IT
+     IT IS CONTROLED BY MERCUNA AI.
+     TO CONTROL DRONE SPEED, PLEASE REFER TO COMPONENT "Mercuna Move controller" ATTACHED TO DRONE"
+    */
 
 
+    
     // Start is called before the first frame update
     void Start()
     {
+        //init
         inputFieldTargetX.text = "X";
         inputFieldTargetY.text = "Y";
         inputFieldTargetZ.text = "Z";
+        inputFieldTargetX.interactable = false;
+        inputFieldTargetY.interactable = false;
+        inputFieldTargetZ.interactable = false;
 
         //Adds a listener to the main input field and invokes a method when the value changes.
-        inputFieldTargetX.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
-        inputFieldTargetY.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
-        inputFieldTargetZ.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        inputFieldTargetX.onEndEdit.AddListener(delegate { ValueChangeCheck(); });
+        inputFieldTargetY.onEndEdit.AddListener(delegate { ValueChangeCheck(); });
+        inputFieldTargetZ.onEndEdit.AddListener(delegate { ValueChangeCheck(); });
 
         m_Toggle = GameObject.Find("ToggleTarget").GetComponent<Toggle>();
-        target = m_Toggle.isOn;
+        target = m_Toggle.isOn;                     //state of the toggle button
+
+        maxSpeedMobileRobot = 8f;                   //define speed of mobile robots
+        //maxSpeedDrone = 15f;
     }
 
+    //function to update values from inputFields
     public void ValueChangeCheck()
     {
+        //converting string to float
         float.TryParse(inputFieldTargetX.text, out SetPosX);
         float.TryParse(inputFieldTargetY.text, out SetPosY);
         float.TryParse(inputFieldTargetZ.text, out SetPosZ);
+
     }
 
-    public void Update()
-    {
-        currentSelection = dropdownHandler.currentSelection;
-        agentCurrentSelection = currentSelection.GetComponent<NavMeshAgent>();
-        ValueChangeCheck();
 
-        target = m_Toggle.isOn;
+    public void SetPosition()
+    {
+        currentSelection = selectionHandler.currentSelection;       //getting selected agent(drone or mobile robot)
+        target = m_Toggle.isOn;                 //state of the toggle button
+        ValueChangeCheck();                     //getting updated value
+
 
         if (target == true)
         {
-            SetPos = new Vector3(SetPosX, SetPosY, SetPosZ);
+            SetPos = new Vector3(SetPosX, SetPosY, SetPosZ);            //Set position to user's input
         }
         else
         {
+            //Set position to current position
             SetPos = new Vector3(currentSelection.transform.position.x, currentSelection.transform.position.y, currentSelection.transform.position.z);
         }
-        
-        //agent.SetDestination(SetPos);
-        agentCurrentSelection.SetDestination(SetPos);
+
+        if (currentSelection.tag == "mobile robots")
+        {
+            if (currentSelection.name == "MobileRobot0")
+            {
+                currentSelection.GetComponent<NavMeshAgent>().enabled = true;
+                /* COMPONENT NavMeshAgent DO NOT ALLOW US TO SPAWN OBJECTS AT RANDOM PLACES.
+                 * SO I ACTIVATE IT ONLY WHEN IT IS NECESSARY.
+                 * ONCE A CLONE SPAWNS, WE CAN ACTIVATE ITS NavMeshAgent COMPONENT
+                 */
+            }
+
+            currentSelection.GetComponent<NavMeshAgent>().SetDestination(SetPos);           //activates pathfinding to set destination
+            currentSelection.GetComponent<NavMeshAgent>().speed = maxSpeedMobileRobot;      //set rover speed
+        }
+
+
+        if (currentSelection.tag == "Drone")
+        {
+            /*FLYING OBJECTS DOESN'T WORK WELL WITH "NavMesh".
+             * FOR DRONES I DECIDED TO USE "Mercuna3DNavigation"
+             * TO CONTROL DRONE SPEED, PLEASE REFER TO COMPONENT "Mercuna Move controller" ATTACHED TO DRONE"
+             */
+            currentSelection.GetComponent<Mercuna.Mercuna3DNavigation>().NavigateToLocation(SetPos);
+
+            
+        }
     }
 }
